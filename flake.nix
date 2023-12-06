24c11948f2a5eb0a4e7ea1a0467bb9fc9c23e101
NuttyShrimp <jan.lecoutere@hotmail.be>
2023-12-06 16:47:16 +0100

    refactor: rewrite flake for nix shell + use rust overlay

--- flake.nix
+++ flake.nix
@@ -3,10 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    devshell = {
-      url = "github:numtide/devshell";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
@@ -14,43 +10,41 @@
       inputs.flake-utils.follows = "flake-utils";
     };
   };
-  outputs = { nixpkgs, flake-utils, devshell, ... }:
+  outputs = { nixpkgs, flake-utils, rust-overlay, ... }:
   flake-utils.lib.eachDefaultSystem (system:
   let
     pkgs = import nixpkgs {
       inherit system;
-      overlays = [ devshell.overlays.default ]; #(import rust-overlay)
+      overlays = [ (import rust-overlay) ];
     };
   in
   with pkgs;
   {
-    devShells.default = pkgs.devshell.mkShell {
+    devShells.default = mkShell rec {
       name = "AudioForge";
-      imports = [ "${devshell}/extra/language/rust.nix" ];
-      packages = [
-        openssl.dev
+      nativeBuildInputs = with pkgs; [
         pkg-config
-        cargo-udeps
-        cargo-watch
-        cargo-limit
-        gcc
 
+        libxkbcommon
+        libGL
+        fontconfig
+
+        # wayland libraries
+        wayland
+
+        # x11 libraries
+        xorg.libXcursor
+        xorg.libXrandr
+        xorg.libXi
+        xorg.libX11
+      ];
+      buildInputs = with pkgs; [
+        (rust-bin.stable.latest.default.override { extensions = [ "rust-analyzer" "rust-src" ]; })
+        openssl
         xorg.libxcb
+        gtkd
       ];
-      env = [
-        {
-          name = "LD_LIBRARY_PATH";
-          value = with pkgs; lib.makeLibraryPath [
-            libGL
-            libxkbcommon
-            wayland
-            xorg.libX11
-            xorg.libXcursor
-            xorg.libXi
-            xorg.libXrandr
-          ];
-        }
-      ];
+      LD_LIBRARY_PATH = "${with pkgs; lib.makeLibraryPath nativeBuildInputs}";
     };
   });
 }
