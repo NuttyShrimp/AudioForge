aeb83995048c37cb21992ce85f207b8596c2ebdb
NuttyShrimp <jan.lecoutere@hotmail.be>
2023-12-27 13:42:57 +0100

    fix(dat54): add default headers constructor + rename properties according to monkeys research

--- src/dat_files/dat54.rs
+++ src/dat_files/dat54.rs
@@ -1,11 +1,12 @@
-#[derive(Default, Debug, serde::Serialize, serde::Deserialize, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Debug, serde::Serialize, serde::Deserialize, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Header {
-    volume: i16, // in dB
-    volume_curve: String,
-    attack_time: Option<i16>,  // Fade-in time
-    release_time: Option<i16>, // Fade-out time
+    volume: i16,          // in dB
+    volume_curve: String, // distance attenuation curves
+    volume_curve_distance: i16,
     doppler_factor: Option<i16>,
     category: String, // Should be prefixed with hash_ following a HEX hash when exporting to xml/dat
+    attack_time: Option<i16>, // Fade-in time
+    release_time: Option<i16>, // Fade-out time
     unk20: u8,        // VirtualiseAsGroup - Stereo panning L-R?
     // - 0 = stereo
     // - 1 = left
@@ -17,4 +18,22 @@ pub struct Header {
     echo_x: Option<i16>,
     echo_y: Option<i16>,
     echo_z: Option<i16>,
+}
+
+impl Default for Header {
+    fn default() -> Self {
+        Self {
+            volume: 100,
+            volume_curve: "C2770146".to_string(),
+            volume_curve_distance: 5,
+            category: "02C7B342".to_string(),
+            doppler_factor: Some(0),
+            attack_time: Some(0),
+            release_time: Some(0),
+            unk20: 3,
+            echo_x: Some(0),
+            echo_y: Some(0),
+            echo_z: Some(0),
+        }
+    }
 }
